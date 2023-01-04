ec0b5753a0176e727ec7e8e26047a0a7ba8a0ca9
max <maximiliangibes@gmail.com>
2023-01-04 06:57:45 -0700

    adds virtual to User.js model

--- models/User.js
+++ models/User.js
@@ -2,30 +2,45 @@ const { Schema, model } = require("mongoose");
 
 //Scheme to create a User model
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            // !! must match a valid email address
-        },
-        thought: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought'
-            }
-        ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ]
-    }
-)
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // !! must match a valid email address
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// create a virtual property "friendCount" that retrieves the length of the user's friends array field on query.
+// !! finish virtual for length of friends
+userSchema
+  .virtual("friendCount")
+  // Getter
+  .get();
+
+module.exports = User;
