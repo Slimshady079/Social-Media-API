ddb0fc59412adc8f6c41fc4ea579b843b7f180e3
max <maximiliangibes@gmail.com>
2023-01-04 06:53:14 -0700

    adds structure to User.js model. Includes username, email, thoughts, friends.

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+//Scheme to create a User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // !! must match a valid email address
+        },
+        thought: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }
+)
