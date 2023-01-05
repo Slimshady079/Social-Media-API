063ffc4f50a094f6304eecb1365ec668e7371b26
max <maximiliangibes@gmail.com>
2023-01-05 07:22:25 -0700

    adds get request for a single user.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,30 @@
 const { User, Thought } = require("../models");
 
 const userController = {
+  // creates a new users
   createUser({ body }, res) {
     // instead of req.body
     User.create(body)
       .then((data) => res.json(data))
       .catch((err) => res.status(500).json(err));
   },
+  // gets all users
   getUsers({ body }, res) {
     User.find()
       .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // gets a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      // * what is populate? It broke my code.
+      // .populate("posts")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
       .catch((err) => res.status(500).json(err));
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
-const { createUser, getUsers } = require("../../controllers/userController");
+const {
+  createUser,
+  getUsers,
+  getSingleUser,
+} = require("../../controllers/userController");
 
+// api/users
+// creates a new user using post request
 router.route("/").post(createUser);
 
+// api/users
+// gets all users using get request
 router.route("/").get(getUsers).post(createUser);
+
+// api/users/:userId
+// gets a single user by their Id using get request
+router.route("/:userId").get(getSingleUser);
 
 module.exports = router;
 
@@ -18,4 +30,3 @@ module.exports = router;
 
 // // api/users/:userId
 // // !! also get populated thought and friend data
-// router.route("/userId").get(getSingleUser);
