21c529a84d29556515bf0250ab678a63a9c65ff7
max <maximiliangibes@gmail.com>
2023-01-05 06:39:37 -0700

    updates userRoutes and userController.

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const { User, Thought } = require("../models");
 
 const userController = {
   createUser({ body }, res) {
+    // instead of req.body
     User.create(body).then((data) => res.json(data));
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,3 +4,17 @@ const { createUser } = require("../../controllers/userController");
 router.route("/").post(createUser);
 
 module.exports = router;
+
+
+// const {
+//   createUser,
+//   getUsers,
+//   getSingleUser,
+// } = require("../../controllers/userController");
+
+// // api/users
+// router.route("/").get(getUsers).post(createUser);
+
+// // api/users/:userId
+// // !! also get populated thought and friend data
+// router.route("/userId").get(getSingleUser);
